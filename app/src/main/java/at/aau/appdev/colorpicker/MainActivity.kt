031c706665a5d38c5decf8d199654ab72e22b779
Daniel Egger <68168885+castanie@users.noreply.github.com>
2025-05-27 19:49:01 +0200

    Create minimal project skeleton (#13)

--- app/src/main/java/at/aau/appdev/colorpicker/MainActivity.kt
+++ app/src/main/java/at/aau/appdev/colorpicker/MainActivity.kt
@@ -7,10 +7,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import at.aau.appdev.colorpicker.camera.CameraScreen
+import at.aau.appdev.colorpicker.detail.DetailScreen
+import at.aau.appdev.colorpicker.gallery.GalleryScreen
 import at.aau.appdev.colorpicker.ui.theme.ColorPickerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +26,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ColorPickerTheme {
+                val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    NavigationHost(
+                        modifier = Modifier.padding(innerPadding), navController = navController
                     )
                 }
             }
@@ -31,17 +38,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun NavigationHost(modifier: Modifier = Modifier, navController: NavHostController) {
+    NavHost(modifier = modifier, navController = navController, startDestination = "camera") {
+        composable(
+            route = "camera", arguments = listOf(navArgument("label") {
+                type = NavType.StringType; defaultValue = "Camera"
+            })
+        ) { CameraScreen(navController) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ColorPickerTheme {
-        Greeting("Android")
+        composable(
+            route = "gallery", arguments = listOf(navArgument("label") {
+                type = NavType.StringType; defaultValue = "Gallery"
+            })
+        ) { DetailScreen(navController) }
+
+        composable(
+            route = "detail/{id}", arguments = listOf(navArgument("label") {
+                type = NavType.StringType; defaultValue = "Detail"
+            }, navArgument("id") {
+                type = NavType.IntType; defaultValue = null
+            })
+        ) { GalleryScreen(navController) }
     }
 }
