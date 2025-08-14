3081587e76c4ab4b6a1f06de4ee1a7514234d245
Daniel Egger <daniel.egger@plincs.com>
2025-08-15 00:55:53 +0200

    Decouple ARCore and Jetpack Compose logic (a bit)

---
+++ app/src/main/java/at/aau/appdev/colorpicker/camera/ARCoreInteractionHandler.kt
@@ -0,0 +1,38 @@
+package at.aau.appdev.colorpicker.camera
+
+import com.google.ar.core.Anchor
+import com.google.ar.core.Frame
+import com.google.ar.core.TrackingState
+import com.google.ar.core.exceptions.NotTrackingException
+
+object ARCoreInteractionHandler {
+
+    fun consumeTapsAndProduceAnchors(
+        frame: Frame,
+        consumeTaps: () -> List<Pair<Float, Float>>,
+        produceAnchors: (List<Anchor>) -> Unit,
+    ) {
+        val taps = consumeTaps()
+        if (taps.isEmpty()) return
+
+        val anchors = mutableListOf<Anchor>()
+
+        for ((x, y) in taps) {
+            val hits = frame.hitTestInstantPlacement(x, y, 3.0f)
+            if (hits.isNotEmpty()) {
+                val hit = hits.first()
+                if (hit.trackable.trackingState == TrackingState.TRACKING) {
+                    try {
+                        anchors += hit.createAnchor()
+                    } catch (_: NotTrackingException) {
+                        // NO-OP.
+                    }
+                }
+            }
+        }
+
+        if (anchors.isEmpty()) return
+        produceAnchors(anchors)
+    }
+
+}
--- app/src/main/java/at/aau/appdev/colorpicker/camera/CameraViewModel.kt
+++ app/src/main/java/at/aau/appdev/colorpicker/camera/CameraViewModel.kt
@@ -1,6 +1,49 @@
 package at.aau.appdev.colorpicker.camera
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import at.aau.appdev.colorpicker.gallery.GalleryUiState
+import at.aau.appdev.colorpicker.persistence.repository.Repository
+import com.google.ar.core.Anchor
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.util.concurrent.ConcurrentLinkedQueue
+import javax.inject.Inject
 
-class CameraViewModel : ViewModel() {
+
+data class CameraUiState(
+    val isLoading: Boolean = true,
+)
+
+class CameraViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
+
+    private val mutableUiState = MutableStateFlow(GalleryUiState())
+    val uiState: StateFlow<GalleryUiState> = mutableUiState.asStateFlow()
+
+    private val mutableAnchors = MutableStateFlow<List<Anchor>>(emptyList())
+    val anchors: StateFlow<List<Anchor>> = mutableAnchors.asStateFlow()
+
+    private val pendingTaps = ConcurrentLinkedQueue<Pair<Float, Float>>()
+
+    fun registerTap(x: Float, y: Float) {
+        pendingTaps.add(Pair(x, y))
+    }
+
+    fun consumeTaps(): List<Pair<Float, Float>> {
+        val taps = mutableListOf<Pair<Float, Float>>()
+        while (true) {
+            taps.add(pendingTaps.poll() ?: break)
+        }
+        return taps
+    }
+
+    fun addAnchors(newAnchors: List<Anchor>) {
+        if (newAnchors.isEmpty()) return
+
+        viewModelScope.launch {
+            mutableAnchors.value = mutableAnchors.value + newAnchors
+        }
+    }
 }
